73bdb1a4ae023f8cbb131ae13e8712fac17f4ff0
lukas <lukas.paczos@gmail.com>
2016-01-27 21:45:33 +0100

    changed package name

---
+++ wordplay/src/main/java/com/paczos/wordplay/GamePanelCross.java
@@ -0,0 +1,14 @@
+package com.paczos.wordplay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GamePanelCross extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_panel_cross);
+    }
+
+}
---
+++ wordplay/src/main/java/com/paczos/wordplay/NewGame.java
@@ -0,0 +1,36 @@
+package com.paczos.wordplay;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NewGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+
+    public void onClick(View view) {
+        Button button = (Button)view;
+        String game = button.getText().toString();
+
+        switch(game) {
+            case "Mastermind" : {
+                Intent intent = new Intent(this, NewMastermind.class);
+                startActivity(intent);
+                finish();
+                break;
+            }
+            case "Cross Words" : {
+                Intent intent = new Intent(this, NewCross.class);
+                startActivity(intent);
+                finish();
+                break;
+            }
+        }
+    }
+}
